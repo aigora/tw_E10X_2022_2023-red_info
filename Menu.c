a9eaaaa7d05ba67fc4e2827c14eac040baf01dc9
InesMelendez <80213891+InesMelendez@users.noreply.github.com>
2023-05-24 22:29:07 +0200

    Leer fichero

--- Menu.c
+++ Menu.c
@@ -3,8 +3,14 @@
 #include "funciones.h"
 #include <time.h>
 #include <string.h>
+
 int main()
-{
+{ 
+    ENERGIA dato[17];
+	CADENAS cadenas[5];
+	FECHAS fechas[25];
+
+    Lee_Fichero(dato, cadenas, fechas);
     Red_Info();
     MenuPrincipal();
 
